1c7a98d47bd3091a1b44a60b6fd0cad904ebad20
René Elbracht <rene.elbracht002@stud.fh-dortmund.de>
2024-09-06 11:36:41 +0200

    make +(add)Button centered

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,18 +1,37 @@
 import { Component } from '@angular/core';
 import {IonicModule} from "@ionic/angular";
-import {fastFoodSharp} from "ionicons/icons";
+import {add, fastFoodSharp} from "ionicons/icons";
 import {addIcons} from "ionicons";
+import {
+  IonApp,
+  IonButton, IonContent,
+  IonFooter,
+  IonHeader,
+  IonIcon,
+  IonRouterOutlet,
+  IonTitle,
+  IonToolbar
+} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
+  styleUrl: './app.component.scss',
   standalone: true,
   imports: [
-    IonicModule
+    IonApp,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonButton,
+    IonIcon,
+    IonRouterOutlet,
+    IonFooter,
+    IonContent
   ],
 })
 export class AppComponent {
   constructor() {
-    addIcons({ fastFoodSharp });
+    addIcons({ fastFoodSharp, add });
   }
 }
