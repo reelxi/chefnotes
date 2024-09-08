eedfe40a4d16139d3e1db1ef0238912e20cd3b34
René Elbracht <rene.elbracht002@stud.fh-dortmund.de>
2024-09-08 23:42:39 +0200

    add primary key

--- Backend/src/recipe_ingredient/recipe_ingredient.entity.ts
+++ Backend/src/recipe_ingredient/recipe_ingredient.entity.ts
@@ -1,10 +1,13 @@
-import { Column, Entity, ManyToOne } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Unit } from '../unit/unit.entity';
 import { Recipe } from '../recipe/recipe.entity';
 import { Ingredient } from '../ingredient/ingredient.entity';
 
 @Entity()
 export class RecipeIngredient {
+  @PrimaryGeneratedColumn()
+  id: number;
+
   @ManyToOne('Recipe')
   recipe: Recipe;
 
