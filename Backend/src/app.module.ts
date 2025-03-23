f6cfeb754a2baff601ad347e88b3c1ea7a4baa28
René Elbracht <elbrachtrene@gmail.com>
2025-03-23 11:30:21 +0100

    🔄️ change: move database.module.ts under /src

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
-import { DatabaseModule } from '../database.module';
+import { DatabaseModule } from './database.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 /**
--- Backend/src/database.module.ts
+++ Backend/src/database.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './src/user/user.entity';
-import { Unit } from './src/unit/unit.entity';
-import { Role } from './src/role/role.entity';
-import { Recipe } from './src/recipe/recipe.entity';
-import { RecipeIngredient } from './src/recipe_ingredient/recipe_ingredient.entity';
-import { DietType } from './src/diet_type/diet_type.entity';
-import { Difficulty } from './src/difficulty/difficulty.entity';
-import { Ingredient } from './src/ingredient/ingredient.entity';
-import { IngredientCategory } from './src/ingredient_category/ingredient_category.entity';
-import { IngredientNutritionalValue } from './src/ingredient_nutritional_value/ingredient_nutritional_value.entity';
-import { InstructionStep } from './src/instruction_step/instruction_step.entity';
+import { User } from './user/user.entity';
+import { Unit } from './unit/unit.entity';
+import { Role } from './role/role.entity';
+import { Recipe } from './recipe/recipe.entity';
+import { RecipeIngredient } from './recipe_ingredient/recipe_ingredient.entity';
+import { DietType } from './diet_type/diet_type.entity';
+import { Difficulty } from './difficulty/difficulty.entity';
+import { Ingredient } from './ingredient/ingredient.entity';
+import { IngredientCategory } from './ingredient_category/ingredient_category.entity';
+import { IngredientNutritionalValue } from './ingredient_nutritional_value/ingredient_nutritional_value.entity';
+import { InstructionStep } from './instruction_step/instruction_step.entity';
 
 /**
  * DatabaseModule encapsulates the database connection and entity registration.
