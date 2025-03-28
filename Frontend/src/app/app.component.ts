3b679abe05f4dfe1c226adcd7c21d7f86492e1c3
René Elbracht <elbrachtrene@gmail.com>
2025-03-28 21:38:23 +0100

    reorganize: into core/feature/shared folder structure

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,32 +1,21 @@
-import { Component } from '@angular/core';
-import {add, fastFoodSharp} from "ionicons/icons";
-import {addIcons} from "ionicons";
-import {
-  IonApp,
-  IonButton, IonContent,
-  IonFooter,
-  IonHeader,
-  IonIcon,
-  IonRouterOutlet,
-  IonTitle,
-  IonToolbar
-} from "@ionic/angular/standalone";
+import { Component } from "@angular/core";
+import { add, fastFoodSharp } from "ionicons/icons";
+import { addIcons } from "ionicons";
+import { IonApp, IonContent, IonRouterOutlet } from "@ionic/angular/standalone";
+import { HeaderComponent } from "./core/components/header/header.component";
+import { FooterComponent } from "./core/components/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrl: './app.component.scss',
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrl: "./app.component.scss",
   standalone: true,
   imports: [
     IonApp,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonButton,
-    IonIcon,
     IonRouterOutlet,
-    IonFooter,
-    IonContent
+    IonContent,
+    HeaderComponent,
+    FooterComponent,
   ],
 })
 export class AppComponent {
